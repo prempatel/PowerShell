1e13a859aa8c84e1d5168e1a0d26df4bb0d67ca3
Prem Patel <12238417+prempatel@users.noreply.github.com>
2019-05-23 12:41:13 -0400

    Add files via upload

--- Get-UsersNotInGroup.ps1
+++ Get-UsersNotInGroup.ps1
@@ -1,6 +1,2 @@
-﻿#Select the group you want
-$group = get-adgroup WF_NJ_SYNC_Users-Desktop
-
-#Get all the users with memberof property, and filter using Where-Object DistinguishedName not in MemberOf
-
-get-aduser -SearchBase "<OU>" -Properties memberof -filter 'enabled -eq $true' | Where-Object {$group.DistinguishedName -notin $_.memberof} | select name | Export-Csv "C:\Users\ppatel-adm\Desktop\WorkFolders.csv"
+﻿$ADGroup = (Get-ADGroup '<GroupName>').DistinguishedName
+Get-ADUser -Filter { memberof -ne $ADGroup -and enabled -eq $true} -SearchBase "<OU>" | select name | Export-Csv C:\Users\ppatel-adm\Desktop\users.csv
