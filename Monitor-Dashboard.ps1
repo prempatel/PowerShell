c37b584ab3b6daa75662a06907b25c9f14ace38a
Prem Patel <12238417+prempatel@users.noreply.github.com>
2019-12-04 14:48:58 -0500

    Add files via upload

---
+++ Monitor-Dashboard.ps1
@@ -0,0 +1,16 @@
+﻿#Auto refresh URL and reopen URL if closed
+
+$link = 'http://nj-vsql-01/Reports/Pages/Report.aspx?ItemPath=%2fGP+Custom+Reports%2fMO+Production+Schedule'
+
+Start-Process iexplore.exe -ArgumentList '-k ', $link
+$wshell = New-Object -ComObject wscript.shell
+
+while($true){
+    do{
+        Start-Sleep 5400
+        $wshell.AppActivate('Internet Explorer')
+        $wshell.SendKeys('{F5}')
+    } while ((Get-Process iexplore -ErrorAction SilentlyContinue).Count -ge 1)
+
+    Start-Process iexplore.exe -ArgumentList '-k', $link
+}
---
+++ Monitor-Process.ps1
@@ -0,0 +1,9 @@
+﻿#Reopen application if closed
+
+Invoke-Item "C:\Users\ppatel\Work Folders\Desktop\TAKT Tracking.accde"
+
+while ($true) {
+    $access = Get-Process msaccess
+    $access.WaitForExit();
+    Invoke-Item "C:\Users\ppatel\Work Folders\Desktop\TAKT Tracking.accde"
+}
