86fbadef12532be96f606ea0b82689aa3d35ad34
Prem Patel <12238417+prempatel@users.noreply.github.com>
2019-12-17 13:33:29 -0500

    Update ReverseDNSLookup.ps1

--- ReverseDNSLookup.ps1
+++ ReverseDNSLookup.ps1
@@ -1,9 +1,12 @@
-﻿$searchIP = Read-Host "Enter IP Address"
-$record = Get-DnsServerResourceRecord -ZoneName "takarabelmont.com" -ComputerName nj-vdc-01 -RRType "A" | where {$_.RecordData.IPV4Address -eq $searchIP} | select hostname, @{ Name = 'IPAddress'; Expression = { $_.RecordData.IPV4Address } }
+# Reverse DNS Lookup in our domain for A records
+ 
+$searchIP = Read-Host "Enter IP Address"
+$record = Get-DnsServerResourceRecord -ZoneName "ZONE_NAME" -ComputerName "DOMAINCONTROLLER" -RRType "A" | where {$_.RecordData.IPV4Address -eq $searchIP} | select hostname, @{ Name = 'IPAddress'; Expression = { $_.RecordData.IPV4Address } }
 
 $hostname = $record | select -Expand hostname
 $ip = $record | select -Expand IPAddress
 $description = Get-ADComputer $hostname -Properties Description | select -Expand Description
+
 Write-Host "Computer: " $hostname
 Write-Host "IP: " $ip
 Write-Host "User: " $description
