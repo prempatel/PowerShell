7b11ccce08cf5e7236a2abf4ac75b84d38a37575
Prem Patel <12238417+prempatel@users.noreply.github.com>
2019-12-17 13:31:28 -0500

    Add files via upload

---
+++ ReverseDNSLookup.ps1
@@ -0,0 +1,12 @@
+﻿$searchIP = Read-Host "Enter IP Address"
+$record = Get-DnsServerResourceRecord -ZoneName "takarabelmont.com" -ComputerName nj-vdc-01 -RRType "A" | where {$_.RecordData.IPV4Address -eq $searchIP} | select hostname, @{ Name = 'IPAddress'; Expression = { $_.RecordData.IPV4Address } }
+
+$hostname = $record | select -Expand hostname
+$ip = $record | select -Expand IPAddress
+$description = Get-ADComputer $hostname -Properties Description | select -Expand Description
+Write-Host "Computer: " $hostname
+Write-Host "IP: " $ip
+Write-Host "User: " $description
+
+#Gridview Version of All DNS Records
+#Get-DnsServerResourceRecord -ZoneName "takarabelmont.com" -ComputerName nj-vdc-01 -RRType "A" | select Hostname, @{ Name = 'IP Address'; Expression = { $_.RecordData.IPV4Address } } | Out-GridView
